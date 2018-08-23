a84003f783b7806791342b08838b99d7f6f4b2b4
krulik <iwillkissyou@gmail.com>
2018-08-23 10:55:58 +0300

    remove mongo calls

--- js-exercises/historical/node/express.js
+++ js-exercises/historical/node/express.js
@@ -1,6 +1,5 @@
 let express = require('express');
 let app = express();
-let mongo = require('mongodb').MongoClient;
 
 app.use(express.static('client'));
 
@@ -13,18 +12,4 @@ app.get('/ancestry/:id', (req, res, next) => {
 
 app.listen(8000, () => {
   console.log('listening on 8000...');
-
-  mongo.connect('mongodb://localhost:27017', (err, client) => {
-    console.log('Connected successfully to database');
-
-    let db = client.db('course');
-    let collection = db.collection('ancestry');
-    collection.insertMany([{name: 'Avishag Krul'}, {name: 'Shalev Krul'}, {name: 'Mira Krul'}, {name: 'Kinneret Krul'}], (err, result) => {
-      console.log(`Inserted ${result.result.n} records`);
-      collection.find({name: 'Serge Krul'}).toArray((err, result) => {
-        console.log(result);
-        client.close();
-      });
-    });
-  });
 });
