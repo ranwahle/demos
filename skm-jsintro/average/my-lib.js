63b39d23bfb30e54813d5673afa86c9420bf75ef
krulik <iwillkissyou@gmail.com>
2018-09-16 12:44:10 +0300

    average lib

---
+++ skm-jsintro/average/my-lib.js
@@ -0,0 +1,14 @@
+let sum = arr => arr.reduce((sum, val) => sum + val, 0);
+let avg = arr => sum(arr) / arr.length;
+let $ = selector => document.querySelector(selector);
+let $$ = selector => document.querySelectorAll(selector);
+
+function calcAverage({button, numbers, output}) {
+  $(button).addEventListener('click', () => {
+    $(output).textContent = avg(
+      Array.from($$(numbers))
+        .map(cell => cell.textContent.match(/\d/g).join(''))
+        .map(Number)
+      );
+  });
+}
