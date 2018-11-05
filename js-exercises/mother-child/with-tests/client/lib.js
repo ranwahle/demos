60626f2835f279e2339f786a3e37e0d975bfc0e7
krulik <iwillkissyou@gmail.com>
2018-11-05 10:01:01 +0200

    add tests

--- js-exercises/mother-child/with-tests/client/lib.js
+++ js-exercises/mother-child/with-tests/client/lib.js
@@ -1,49 +1,42 @@
-(function () {
-
-  'use strict';
-
-  let lib = {
+if (typeof module !== 'undefined' && typeof module.exports === 'object') {
+  module.exports = {
     getAverage,
     average,
     createHash,
     request
   };
+}
 
-  if (typeof module !== 'undefined' && typeof module.exports === 'object') {
-    module.exports = lib;
-  } else {
-    window.lib = lib;
+function getAverage(list) {
+  let byName = createHash(list, 'name');
+  let withMothers = list.filter(p => Boolean(byName[p.mother]));
+  let ageDiff = p => p.born - byName[p.mother].born;
+  let ageDiffs = withMothers.map(ageDiff);
+  return average(ageDiffs);
+}
+
+function average(list) {
+  if (!list.length) {
+    return null;
   }
+  return list.reduce((sum, value) => sum + value, 0) / list.length;
+}
 
-  function getAverage(list) {
-    let byName = createHash(list, 'name');
-    let withMothers = list.filter(p => Boolean(byName[p.mother]));
-    let ageDiff = p => p.born - byName[p.mother].born;
-    let ageDiffs = withMothers.map(ageDiff);
-    return average(ageDiffs);
-  }
+function createHash(list, key) {
+  return list.reduce((hash, item) => {
+    hash[item[key]] = item;
+    return hash;
+  }, {});
+}
 
-  function average(list) {
-    return list.reduce((sum, value) => sum + value, 0) / list.length;
-  }
-
-  function createHash(list, key) {
-    return list.reduce((hash, item) => {
-      hash[item[key]] = item;
-      return hash;
-    }, {});
-  }
-
-  function request(method, url, resolve, reject) {
-    let xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.addEventListener('load', e => {
-      resolve(JSON.parse(e.target.responseText))
-    });
-    xhr.addEventListener('error', err => {
-      reject(err);
-    });
-    xhr.send();
-  }
-
-}());
+function request(method, url, resolve, reject) {
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.addEventListener('load', e => {
+    resolve(JSON.parse(e.target.responseText))
+  });
+  xhr.addEventListener('error', err => {
+    reject(err);
+  });
+  xhr.send();
+}
--- js-exercises/mother-child/with-tests/client/lib.test.js
+++ js-exercises/mother-child/with-tests/client/lib.test.js
@@ -1,46 +1,63 @@
-let lib, sinon;
-if (typeof require === 'function') {
-  lib = require('./lib');
-  sinon = require('sinon');
-} else {
-  lib = window.lib;
-}
+let lib = require('./lib');
 
-describe('Logic', () => {
+describe('Helpers', () => {
   describe('average', () => {
     it('should calc average', () => {
       expect(lib.average([1, 2, 3])).toBe(2);
     });
+    it('should handle empty input', () => {
+      expect(lib.average([])).toBe(null);
+    })
+  });
+
+  describe('createHash', () => {
+    it('should return a hash by provided key', () => {
+      let data = [
+        {name: 'AAA', born: 1900},
+        {name: 'BBB', born: 2000},
+      ];
+      let nameHash = lib.createHash(data, 'name');
+      expect(nameHash['AAA'].born).toBe(1900);
+      expect(nameHash['BBB'].born).toBe(2000);
+
+      let bornHash = lib.createHash(data, 'born');
+      expect(bornHash[1900].name).toBe('AAA');
+      expect(bornHash[2000].name).toBe('BBB');
+    });
   });
 });
 
-describe('API', () => {
-  let server;
-  beforeAll(() => {
-    server = sinon.fakeServer.create();
-  });
-  afterAll(() => {
-    server.restore();
-  });
-  describe('request', () => {
-    it('should execute resolve callback', (done) => {
-      let callback = jest.fn();
-      let result = [{name: 'Serge'}];
-      server.respondWith('GET', '/ancestry', JSON.stringify(result));
-      lib.request('GET', '/ancestry', callback);
-      server.respond();
-      expect(callback).toHaveBeenCalledWith(result);
-      done();
+describe('Ancestry logic', () => {
+  describe('getAverage', () => {
+    it('should return mother-child age diffs', () => {
+      let data = [
+        {name: 'AAA', born: 1900, mother: 'AAA_M'},
+        {name: 'AAA_M', born: 1850, mother: null},
+        {name: 'BBB', born: 1900, mother: 'BBB_M'},
+        {name: 'BBB_M', born: 1800, mother: null}
+      ];
+      let result = lib.getAverage(data);
+      expect(result).toBe(75);
     });
-    it('should execute reject callback', (done) => {
-      let callback = jest.fn();
-      server.respondWith('GET', '/ancestry', request => {
-        request.error();
-      });
-      lib.request('GET', '/ancestry', null, callback);
-      server.respond();
-      expect(callback).toHaveBeenCalled();
-      done();
-    })
+    it('should handle null input', () => {
+      let data = [
+        {name: 'AAA', born: 1900, mother: null},
+        {name: 'AAA_M', born: 1850, mother: null},
+        {name: 'BBB', born: 1900, mother: null},
+        {name: 'BBB_M', born: 1800, mother: null}
+      ];
+      let result = lib.getAverage(data);
+      expect(result).toBe(null);
+    });
+    it('should ignore when mother younger than child', () => {
+      let data = [
+        {name: 'AAA', born: 1900, mother: 'AAA_M'},
+        {name: 'AAA_M', born: 1950, mother: null},
+        {name: 'BBB', born: 1900, mother: 'BBB_M'},
+        {name: 'BBB_M', born: 1800, mother: null}
+      ];
+      let result = lib.getAverage(data);
+      expect(result).toBe(25);
+    });
   });
 });
