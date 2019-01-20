f7b0b94256abd73e8a9cd26785a526581654b799
krulik <iwillkissyou@gmail.com>
2019-01-20 16:17:17 +0200

    remove bored from demos

--- skm-ci/bored/client/client.js
+++
@@ -1,7 +0,0 @@
-main();
-
-async function main() {
-  let response = await fetch('/activity');
-  let activity = await response.json();
-  document.querySelector('main').innerHTML = activity.activity;
-}
