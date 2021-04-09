e996cd91e01772e1605d6354cb672d1b621115fb
krulik <iwillkissyou@gmail.com>
2021-04-09 11:23:09 +0300

    do phase 3 in class (until active tab)

--- todo/dom-3/main.js
+++ todo/dom-3/main.js
@@ -1,13 +1,14 @@
 var Input = document.querySelector('.TodoInput');
-var ListContainer = document.querySelector('.ListContainer');
 
 var todo = [
-  { id: 'id-Sleep', name: "Sleep", active: true},
-  { id: 'id-Shopping', name: "Shopping", active: false},
-  { id: 'id-Bank', name: "Bank", active: true}
+  { id: getItemId("Sleep"), name: "Sleep", active: true},
+  { id: getItemId("Shopping"), name: "Shopping", active: false},
+  { id: getItemId("Bank"), name: "Bank", active: true}
 ];
 
-render(todo, 'onLoad');
+var selectedTab = 'all';
+
+render(todo, selectedTab);
 
 Input.addEventListener('change', onInputChange);
 
@@ -15,37 +16,107 @@ Input.addEventListener('change', onInputChange);
 // - Actions ------------------------------------------------
 
 function onInputChange() {
-  addItemToList(Input.value);
-  render(todo, 'onInputChange');
+  // add item to the array list
+  addItemToList(todo, Input.value);
+  // use existing method of rendering the full array list
+  render(todo, selectedTab);
 }
 
-function onItemToggle(e) {
-  for (var item of todo) {
-    if (item.id === e.target.id) {
-      toggleItemStatus(item);
+function onCheckboxChange(eventObject) {
+  // toggle item status in array
+  for (var i = 0; i < todo.length; i++) {
+    if (todo[i].id === eventObject.target.id) {
+      toggleItemStatus(todo[i]);
     }
   }
-  render(todo, 'onItemToggle');
+  // render the updated array
+  render(todo, selectedTab);
 }
 
-function onItemRemove(e) {
-  for (var item of todo) {
-    if (item.id === e.target.id) {
-      deleteItemFromList(item);
+function onRemoveButtonClick(eventObject) {
+  // remove item from array
+  for (var i = 0; i < todo.length; i++) {
+    if (todo[i].id === eventObject.target.id) {
+      deleteItemFromList(todo, todo[i]);
     }
   }
-  render(todo, 'onItemRemove');
+  // render the updated array
+  render(todo, selectedTab);
 }
+
+// 1. action
+function onTabClick(eventObject) {
+  // 2. payload: find out which tab was clicked
+  // 3. update our state - ?
+  selectedTab = eventObject.target.id;
+  // filter the todo array according to selectedTab
+  // 4. render
+  // render only the filtered todo array
+  render(todo, selectedTab);
+}
+
 
 // - View ------------------------------------------------
-
-function render(todo, actionName) {
-  console.log(actionName, JSON.stringify(todo));
+function render(todo, selectedTab) {
+  var ListContainer = document.querySelector('.ListContainer');
+  var FooterContainer = document.querySelector('.FooterContainer');
   var List = document.querySelector('.List');
   if (List) {
     ListContainer.removeChild(List);
   }
   ListContainer.appendChild(TodoList(todo));
+  var Footer = document.querySelector('footer');
+  if (Footer) {
+    FooterContainer.removeChild(Footer);
+  }
+  FooterContainer.appendChild(TodoFooter(todo, selectedTab));
+}
+
+function TodoFooter(todoArray, selectedTab) {
+  // <footer class="Item Item--full flex flex-justify--between text-small">
+  //   <span>2 items left</span>
+  //   <ul class="flex flex-gapRight--small">
+  //     <li><a class="Tab" id="all" href="#all">All</a></li>
+  //     <li><a class="Tab" id="active" href="#active">Active</a></li>
+  //     <li><a class="Tab" id="completed" href="#completed">Completed</a></li>
+  //   </ul>
+  //   <button class="text-decoration:hover">Clear completed</button>
+  // </footer>
+
+  var footer = document.createElement('footer');
+  footer.className = 'Item Item--full flex flex-justify--between text-small';
+
+  var span = document.createElement('span');
+  span.innerText = getActiveItems(todoArray).length + ' items left';
+
+  var ul = document.createElement('ul');
+  ul.className = 'flex flex-gapRight--small';
+
+  var tabs = ['All', 'Active', 'Completed'];
+  for (var i = 0; i < tabs.length; i++) {
+    var tabId = tabs[i].toLowerCase();
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    a.className = 'Tab';
+    if (tabId === selectedTab) {
+      a.className += ' is-selected';
+    }
+    a.innerText = tabs[i];
+    a.id = tabId;
+    a.href = '#' + tabId;
+    a.addEventListener('click', onTabClick);
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+
+  var button = document.createElement('button');
+  button.className = 'text-decoration:hover';
+  button.innerText = 'Clear completed';
+
+  footer.appendChild(span);
+  footer.appendChild(ul);
+  footer.appendChild(button);
+  return footer;
 }
 
 function TodoList(todoArray) {
@@ -64,39 +135,57 @@ function TodoItem(todoItem) {
   //   <label for="id-1">foo</label>
   //   <button class="Item-remove flex-alignSelf--right" aria-label="Remove">&times;</button>
   // </li>
-  var newId = 'id-' + todoItem.name;
+
   var li = document.createElement('li');
   li.className = 'Item flex'
-  var input = document.createElement('input');
-  input.type = 'checkbox';
-  input.id = newId;
-  input.checked = todoItem.active === false;
-  input.addEventListener('click', onItemToggle)
+
+  var checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.id = getItemId(todoItem.name);
+  checkbox.checked = todoItem.active === false;
+  checkbox.addEventListener('change', onCheckboxChange);
+
   var label = document.createElement('label');
-  label.htmlFor = newId;
+  label.htmlFor = getItemId(todoItem.name);
   label.innerText = todoItem.name;
-  var button = document.createElement('button');
-  button.className = 'Item-remove flex-alignSelf--right';
-  button.setAttribute('aria-label', 'Remove');
-  button.innerHTML = '&times;';
-  button.id = newId;
-  button.addEventListener('click', onItemRemove);
-  li.appendChild(input);
+
+  var removeButton = document.createElement('button');
+  removeButton.id = getItemId(todoItem.name);
+  removeButton.className = 'Item-remove flex-alignSelf--right';
+  removeButton.setAttribute('aria-label', 'Remove');
+  removeButton.innerHTML = '&times;';
+  removeButton.addEventListener('click', onRemoveButtonClick);
+
+  li.appendChild(checkbox);
   li.appendChild(label);
-  li.appendChild(button);
+  li.appendChild(removeButton);
   return li;
 }
 
-// - Store ------------------------------------------------
-function addItemToList(name){
-  var newItem = {id: 'id-' + name, name: name, active: true};
+
+// - Todo Model ------------------------------------------------
+
+function getItemId(name) {
+  return 'id-' + name;
+}
+function getActiveItems(todo) {
+  var activeItems = [];
+  for (var i = 0; i < todo.length; i++) {
+    if (todo[i].active) {
+      activeItems.push(todo[i]);
+    }
+  }
+  return activeItems;
+}
+
+function addItemToList(todo, name){
+  var newItem = {name: name, active: true, id: getItemId(name)};
   todo.push(newItem);
 }
 
-function deleteItemFromList(item){
+function deleteItemFromList(todo, item){
   // getting the item's index
   var idx = todo.indexOf(item);
-
   // deleting the item from the array
   todo.splice(idx, 1);
 }
@@ -105,6 +194,6 @@ function renameItem(item, newName){
   item.name = newName;
 }
 
-function toggleItemStatus(item){
+function toggleItemStatus (item){
   item.active = !item.active;
 }
