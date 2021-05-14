5e26b573666a1c238351d3c7c669bf1cc0edce12
krulik <iwillkissyou@gmail.com>
2021-05-14 10:27:30 +0300

    phase 1 products with react

--- react/compare-product/src/index.js
+++ react/compare-product/src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { PRODUCTS } from './products';
 
 ReactDOM.render(
   <App />,
@@ -7,5 +8,23 @@ ReactDOM.render(
 );
 
 function App() {
-  return <main>Hello World</main>;
+  let [state, setState] = useState({products: PRODUCTS, selectedKeys: ['t_shirt', 'shoe']})
+  return <main className="u-p-large">
+      <List products={state.products} selectedKeys={state.selectedKeys} />
+    </main>;
+}
+
+function List({products, selectedKeys}) {
+  let isSelected = (p) => selectedKeys.includes(p.key);
+  return <ol className="List">
+      {products.map(p => <li><Product product={p} isSelected={isSelected(p)} /></li>)}
+    </ol>;
+}
+
+function Product({product, isSelected}) {
+  return <div className={`Product ${isSelected && 'is-selected'}`}>
+    <span>{product.name}</span>
+    <img src={`img/${product.key}.png`} />
+    <p>{product.price}</p>
+  </div>;
 }
