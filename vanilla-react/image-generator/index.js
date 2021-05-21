e6c71bd5d41b51ac60b31a25d968e230018bb67b
krulik <iwillkissyou@gmail.com>
2021-05-21 12:05:22 +0300

    add category

--- vanilla-react/image-generator/index.js
+++ vanilla-react/image-generator/index.js
@@ -27,8 +27,13 @@ function onFilterChange(event) {
   setState({filter: event.target.value});
 }
 
+function onCategoryChange(event) {
+  let select = event.target;
+  let option = select.options[select.selectedIndex];
+  setState({category: option.value});
+}
+
 function App(state) {
-  let url = `https://placeimg.com/${state.size.w}/${state.size.h}/${state.category}${state.filter && `/${state.filter}`}`;
   return `
     <main style="
       height: 100vh;
@@ -38,18 +43,47 @@ function App(state) {
       flex-direction: column;
       gap: 1em;">
 
-      <img
-        style="border: 1px dashed"
-        width="${state.size.w}" height="${state.size.h}"
-        src="${url}" />
-
-      <p>${url}</p>
+      ${Image(state)}
 
       <div>
         <button onclick="onGenerate(event)">Generate</button>
-        <input onclick="onFilterChange(event)" type="radio" ${state.filter === 'sepia' && 'checked'} value="sepia" id="sepia"><label for="sepia">Sepia</label>
-        <input onclick="onFilterChange(event)" type="radio" ${state.filter === 'grayscale' && 'checked'} value="grayscale" id="grayscale"><label for="grayscale">Grayscale</label>
+        ${Filters(state.filter)}
+        ${Categories(state.category)}
       </div>
     </main>
+  `;
+}
+
+function Image(state) {
+  let url = `https://placeimg.com/${state.size.w}/${state.size.h}/${state.category}${state.filter && `/${state.filter}`}`;
+  return `
+    <img
+      style="border: 1px dashed"
+      width="${state.size.w}" height="${state.size.h}"
+      src="${url}" />
+
+    <p>${url}</p>
+  `;
+}
+
+function Filters(filter) {
+  return `
+    <input onclick="onFilterChange(event)" type="radio" ${filter === 'sepia' && 'checked'} value="sepia" id="sepia"><label for="sepia">Sepia</label>
+    <input onclick="onFilterChange(event)" type="radio" ${filter === 'grayscale' && 'checked'} value="grayscale" id="grayscale"><label for="grayscale">Grayscale</label>
+  `;
+}
+
+function Categories(category) {
+  let options = [
+    {value: 'animals'},
+    {value: 'people'},
+    {value: 'nature'},
+    {value: 'tech'},
+    {value: 'architecture'}
+  ];
+  return `
+    <select onchange="onCategoryChange(event)">
+      ${options.map(option => `<option value="${option.value}" ${option.value === category && 'selected'}>${option.value}</option>`).join('')}
+    </select>
   `;
 }
