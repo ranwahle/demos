ddd31ac114ac94a85a982a5dff225a84c531eaa6
krulik <iwillkissyou@gmail.com>
2021-06-04 11:51:27 +0300

    add comments on click

--- whatsapp-react/src/index.js
+++ whatsapp-react/src/index.js
@@ -8,21 +8,55 @@ ReactDOM.render(
 
 function App() {
   let [posts, setPosts] = useState([]);
+  let [selectedId, setSelectedId] = useState(1);
+  let [comments, setComments] = useState([]);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
-      .then(postsRes => setPosts(postsRes));
+      .then((posts) => setPosts(posts))
   }, []);
+
+  useEffect(() => {
+    if (!selectedId) {
+      return;
+    }
+    fetch(`https://jsonplaceholder.typicode.com/comments?postId=${selectedId}`)
+      .then(res => res.json())
+      .then((comments) => setComments(comments))
+  }, [selectedId]);
+
+  let selectedPost = posts.find((p) => p.id === selectedId);
 
   return <Panes>
     <Pane width={'35%'} minWidth={'300px'}>
-      <Posts posts={posts}></Posts>
+      <Posts posts={posts} onSelectPost={setSelectedId}></Posts>
     </Pane>
     <Pane width={'65%'}>
-      here be comments
+      <Comments post={selectedPost} comments={comments}></Comments>
     </Pane>
   </Panes>;
+}
+
+function Comments(props) {
+  if (!props.post || !props.comments) {
+    return '';
+  }
+  let commentStyle = {
+    border: '1px solid',
+    padding: '0.5em'
+  };
+  return <div>
+    <h2>{props.post.title} ({props.post.id})</h2>
+    <ul>
+      {props.comments.map(comment => <div key={comment.id} style={commentStyle}>
+        <p>postId: {comment.postId}</p>
+        <p>name: {comment.name}</p>
+        <p>email: {comment.email}</p>
+        <p>{comment.body}</p>
+      </div>)}
+    </ul>
+  </div>
 }
 
 function Posts(props) {
@@ -33,7 +67,7 @@ function Posts(props) {
   }
   return <ul>
     {props.posts.map(post => {
-      return <div style={postStyle} key={post.id}>
+      return <div style={postStyle} key={post.id} onClick={() => props.onSelectPost(post.id)}>
         {post.title} ({post.id})
       </div>
     })}
