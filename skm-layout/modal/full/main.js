d7d79cde5f63ed1d727e1d3d4231ce17dcc042fb
krulik <iwillkissyou@gmail.com>
2022-01-14 12:51:36 +0200

    enhance modal with escape

--- skm-layout/modal/full/main.js
+++ skm-layout/modal/full/main.js
@@ -1,21 +1,36 @@
 const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
-const open = document.querySelector('.modal-open');
+const openButtons = document.querySelectorAll('.modal-open');
 const cancel = document.querySelector('.modal-cancel');
 const close = document.querySelector('.modal-close');
 const submit = document.querySelector('.modal-submit');
 
-open.addEventListener('click', show);
+openButtons.forEach(openButton => {
+  openButton.addEventListener('click', show);
+});
 cancel.addEventListener('click', hide);
 close.addEventListener('click', hide);
 submit.addEventListener('click', hide);
+document.addEventListener('keyup', (e) => {
+  if (e.key === 'Escape') {
+    hide();
+  }
+});
 
 function show() {
   modal.classList.remove('is-hidden');
-  body.classList.add('is-modal-open');
+  disableScroll();
 }
 
 function hide() {
   modal.classList.add('is-hidden');
+  enableScroll();
+}
+
+function disableScroll() {
+  body.classList.add('is-modal-open');
+}
+
+function enableScroll() {
   body.classList.remove('is-modal-open');
 }
